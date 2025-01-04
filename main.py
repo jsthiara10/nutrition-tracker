4c5a5556ed152f27ab11558e66968920704aa678
jsthiara10 <86239238+jsthiara10@users.noreply.github.com>
2025-01-04 13:50:23 +0000

    function to allow user to change calorie goal
    calorie goal - if user changes their mind e.g. 2500 cals instead of 2000, there is a conditional now which allows them to change their mind

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def goal_setter():
                 print("Exiting program. Goodbye! ")
                 break
         except ValueError:
-            print("Please enter a number e.g. 1 for Calories")
+            print("Please enter a number e.g. 1 for Calories") 
     
         break
 
@@ -47,33 +47,42 @@ def calorie_goal():
                 print("Calorie goal cannot be less than 0 Calories")
                 continue
             elif goal >=0:
-                print(f"You have entered {goal} as your daily calorie goal") # enter an option here to ask if they are sure/go back and edit calorie goal
-                meals()
+                confirmation = input(f"You have entered {goal} as your daily calories. Are you sure? Yes or No? ").strip().lower()
+                if confirmation == "yes":
+                    print(f"Your calorie goal is set to {goal} calories. ")
+                    meals()
+                    return goal # ends the execution of the function
+                elif confirmation == "no":
+                    print("Please enter your updated calorie goal. ")
+                    continue
+                else:
+                    print("Invalid input. Please enter Yes or No. ")
         except ValueError:
             print("You must enter a positive number e.g. 10 or 10.5")
+        break
 
 def meals():
     global calories
     while True:
         try:
-            meal = goal = float(input("Please enter calories in your first meal"))
+            meal = float(input("Please enter calories in your first meal: "))
             if meal < 0:
                 print("Meal must have at least 0 calories")
                 continue
             elif meal > 0:
-                print(f"You have entered {meal} calories for your first meal")
-                calories += meal
+                confirmation = input(f"You have entered {meal} as calories for your first meal. Are you sure? Please enter yes or no" ).strip().lower()
+                if confirmation == "yes":
+                    print(f"You have confirmed {meal} calories for your first meal")
+                    calories += meal
                 print(f"You have consumed {calories} calories so far today")
+                # Now, we want to ask them do you wish to record another meal?
+                # If the answer is yes, then go ahead and ask them to record another meal
+                add_meal()
         except ValueError:
             print("You must enter a number greater than 0 for calories")
 
-
-
-# def macronutrients():
-
-
-
-
+def add_meal():
+    print("test")
 
     # while True:
     #     protein = float(input("Please enter how many g of protein you would like to consume to day e.g. 150g"))
@@ -83,7 +92,9 @@ def meals():
 
 
 def main():
+    global calories
     print("Welcome to JST's Nutrition Tracker")
     goal_setter()
+    print(f"You consumed {calories} calories today")
 
 main() # start of the program
