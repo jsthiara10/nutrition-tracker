f3dedaaacacfe7dca68ccd5babde2093ce12c01c
jsthiara10 <86239238+jsthiara10@users.noreply.github.com>
2025-01-04 16:09:50 +0000

    Create practice.py
    practice environment for new features. this will be added to gitignore soon

---
+++ practice.py
@@ -0,0 +1,20 @@
+# I want to set a calorie goal
+
+# I want to see as a percentage of that goal, what my total calorie intake is 
+
+goal = 2000
+
+consumed = 500
+
+percentage = (consumed/goal) * 100
+
+print(f"You are {percentage}% of the way towards your daily calorie goal! ")
+
+# Subtraction
+
+subtraction = goal - consumed
+
+print(f"You have {subtraction} calories left to consume today. ")
+
+# End message
+
